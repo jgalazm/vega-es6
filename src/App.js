342e65af58f78eae457911ca00fc7f11c3b0f1c5
José  Galaz <jdgalazm@gmail.com>
2019-01-25 18:42:47 -0300

    add basic example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as vegal from 'vega-lite';
+import * as vega from 'vega';
+import vegae from 'vega-embed';
 
 class App extends Component {
+
+  componentDidMount(){
+    var yourVlSpec = {
+      $schema: 'https://vega.github.io/schema/vega-lite/v2.0.json',
+      description: 'A simple bar chart with embedded data.',
+      data: {
+        values: [
+          {a: 'A', b: 28},
+          {a: 'B', b: 55},
+          {a: 'C', b: 43},
+          {a: 'D', b: 91},
+          {a: 'E', b: 81},
+          {a: 'F', b: 53},
+          {a: 'G', b: 19},
+          {a: 'H', b: 87},
+          {a: 'I', b: 52}
+        ]
+      },
+      mark: 'bar',
+      encoding: {
+        x: {field: 'a', type: 'ordinal'},
+        y: {field: 'b', type: 'quantitative'}
+      }
+    };
+    vegae('#vis', yourVlSpec);    
+  }
+
   render() {
+    
+    console.log(vega,vegal,vegae);
+    debugger;
     return (
       <div className="App">
         <header className="App-header">
@@ -20,6 +53,8 @@ class App extends Component {
             Learn React
           </a>
         </header>
+
+        <div id="vis"></div>
       </div>
     );
   }
